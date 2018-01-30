3ec73c11c1b0400d059e57f307737ed8c0df50ac
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2018-01-29 20:05:29 -0500

    basic scene transition

--- MAPD724 - Lesson 2- SpriteKit/Scenes/StartScene.swift
+++ MAPD724 - Lesson 2- SpriteKit/Scenes/StartScene.swift
@@ -14,4 +14,14 @@ class StartScene: SKScene {
         
         let welcomeLabel: SKLabelNode = self.childNode(withName: "welcomeLabel") as! SKLabelNode
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let scene = SKScene(fileNamed: "GameScene") {
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view?.presentScene(scene, transition: SKTransition.crossFade(withDuration: 1.0))
+        }
+    }
 }
